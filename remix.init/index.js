77273e07e41fa54770aa93541a6e67ddce293d66
Yaobin <me@yaobin.me>
2022-05-21 14:46:10 +0800

    chore: Remove lockfiles

--- remix.init/index.js
+++ remix.init/index.js
@@ -35,11 +35,6 @@ async function main({ rootDirectory }) {
     fs.readFile(README_PATH, "utf-8"),
     fs.readFile(EXAMPLE_ENV_PATH, "utf-8"),
     fs.readFile(PACKAGE_JSON_PATH, "utf-8"),
-    fs.rm(path.join(rootDirectory, ".github/ISSUE_TEMPLATE"), {
-      recursive: true,
-    }),
-    fs.rm(path.join(rootDirectory, ".github/PULL_REQUEST_TEMPLATE.md")),
-    fs.rm(path.join(rootDirectory, ".github/FUNDING.yml")),
   ]);
 
   const newEnv = env.replace(
@@ -67,6 +62,15 @@ async function main({ rootDirectory }) {
     fs.writeFile(README_PATH, newReadme),
     fs.writeFile(ENV_PATH, newEnv),
     fs.writeFile(PACKAGE_JSON_PATH, newPackageJson),
+    fs.copyFile(
+      path.join(rootDirectory, "remix.init", "gitignore"),
+      path.join(rootDirectory, ".gitignore")
+    ),
+    fs.rm(path.join(rootDirectory, ".github/ISSUE_TEMPLATE"), {
+      recursive: true,
+    }),
+    fs.rm(path.join(rootDirectory, ".github/PULL_REQUEST_TEMPLATE.md")),
+    fs.rm(path.join(rootDirectory, ".github/FUNDING.yml")),
   ]);
 
   console.log(
