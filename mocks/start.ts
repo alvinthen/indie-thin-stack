e322fe218d82e8713e24025dae0d5136c8b299c5
Yaobin <me@yaobin.me>
2022-05-21 22:12:25 +0800

    chore: Re-add ts-node to use TypeScript for msw

---
+++ mocks/start.ts
@@ -0,0 +1,11 @@
+import { setupServer } from 'msw/node';
+import '~/utils';
+import { userHandlers } from './user.mock';
+
+const server = setupServer(...userHandlers);
+
+server.listen({ onUnhandledRequest: 'warn' });
+console.info('🔶 Mock server running');
+
+process.once('SIGINT', () => server.close());
+process.once('SIGTERM', () => server.close());
